884f366586d4ca7abe6f4b43d8962eeae5da23aa
Sajan Maharjan <itsmesajanmaharjan@gmail.com>
2025-01-25 14:42:10 +0545

    add button

---
+++ Based-Project/script.js
@@ -0,0 +1,4 @@
+document.getElementById("homeButton").addEventListener("click", () => {
+  // Replace 'index.html' with the home page URL of your website
+  window.location.href = "https://www.maharjan-sajan.com.np/";
+});
